ead388632ced81d417b5e440513ddfd61fa13622
Gym-Mentor <61284215+Gym-Mentor@users.noreply.github.com>
2020-07-17 22:52:27 +0900

    Add files via upload

--- 10844쉬운계단수.cpp
+++ 10844쉬운계단수.cpp
@@ -4,24 +4,23 @@ int Dp[101][10];
 int main() {
 	int N;
 	cin >> N;
-	Dp[0][0] = 0;
-	for (int i = 1; i < 10; i++)
-	{
+	for (int i = 0; i < 10; i++) {
 		Dp[0][i] = 1;
 	}
 	for (int i = 1; i < N; i++)
 	{
-		Dp[i][0] = Dp[i - 1][1];
-		Dp[i][9] = Dp[i - 1][8];
-		for (int j = 1; j < 9; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			Dp[i][j] = Dp[i - 1][j - 1] + Dp[i - 1][j + 1];
+			for (int k = j; k < 10; k++)
+			{
+				Dp[i][j] = (Dp[i][j] + Dp[i - 1][k]) % 10007;
+			}
 		}
 	}
 	int sum = 0;
 	for (int i = 0; i < 10; i++)
 	{
-		sum += Dp[N - 1][i];
+		sum+=Dp[N - 1][i];
 	}
 	cout << sum;
 }
