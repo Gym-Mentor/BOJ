7587eb33cf75e6f50a3fc5d6435e34bcdd6164e7
Gym-Mentor <61284215+Gym-Mentor@users.noreply.github.com>
2020-07-29 22:40:46 +0900

    Add files via upload

---
+++ 11055가장큰증가부분수열.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int dp[1001], a[1001];
+	int max = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int min = 0;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j] < a[i])
+				if (min < dp[j])
+					min = dp[j];
+		}
+		dp[i] = min + a[i];
+		if (max < dp[i])
+			max = dp[i];
+	}
+	cout << max;
+}
